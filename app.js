40f8aa45607863d18a75319747cfab19e45c59a0
rohnshrma <rohn11shrma@gmail.com>
2022-07-12 16:39:06 +0530

    finished secrets

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const passportLocal = require("passport-local");
 const session = require("express-session");
+var findOrCreate = require('mongoose-findorcreate')
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,21 +47,60 @@ const userSchema = new mongoose.Schema({
     // required: true,
     type: String,
   },
+  googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id); 
+ // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+      done(err, user);
+  });
+});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:5500/auth/google/secrets",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 // routes
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  });
+
 
 app.get("/register", (req, res) => {
   res.render("register");
@@ -118,18 +159,14 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-
-  req.logout((err)=>{
-if(err){
-
-  console.log(err);
-}else{
-res.redirect("/login")  
-}
-
-  })
+  req.logout((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/login");
+    }
+  });
 });
-
 
 app.listen(5500, () => {
   console.log("server started on port : 5500");
