c2aaa47661ee5fbbd38a008f38d1bc0d0159ab3c
rohnshrma <rohn11shrma@gmail.com>
2022-07-13 18:06:27 +0530

    changes finally

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:5500/auth/google/secrets"
+  callbackURL: "https://udit-auth.herokuapp.com/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
 
